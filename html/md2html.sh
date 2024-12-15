b3e99d35740da30b658d04454fae99491fc726f9
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-12-15 23:41:27 +0100

    md2html.sh: improved, it creates index.html as well
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- html/md2html.sh
+++ html/md2html.sh
@@ -3,6 +3,62 @@
 # (C) 2024, Roberto A. Foglietta <roberto.foglietta@gmail.com> - 3-clause BSD
 #
 
+function md2htmlfunc() {
+    local str;
+    printf "<html>
+    <head>
+        <title>$(basename ${2%.html})</title>
+        <style>
+$(cat html/default.css)
+        </style>
+        <link rel='stylesheet' href='custom.css'>
+    </head>
+    <body>
+" >$2
+    sed -e "s,§,\sect;,g" \
+-e "s,^# \(.*\),<H1>\\1</H1>," \
+-e "s,^## \(.*\),<H2>\\1</H2>," \
+-e "s,^### \(.*\),<H3>\\1</H3>," \
+-e "s,^#### \(.*\),<H4>\\1</H4>," \
+-e "s,^##### \(.*\),<H5>\\1</H5>," \
+-e "s,^ *[-+\*] *> *\(.*\),<blockquote><li>\\1</li></blockquote>," \
+-e "s,^> \(.*\),<blockquote>\\1</blockquote>," \
+-e "s,^ *[-+\*] \(.*\),<li>\\1</li>," \
+-e "s,^ *\([0-9]*\)\. \(.*\),<li style='list-style-type: none;'><b>\\1.</b><span>\&nbsp;\&nbsp;\&nbsp;</span>\\2</li>," \
+-e 's,\[\([^]]*\)\](\([^)]*\)),<a href="\2">\1<\/a>,g' \
+-e "s,\\\<\(.*\)\\\>,\&lt;\\1\&gt;,g" \
+-e "s,^ *$,<p/>," -e "s,^---.*,<hr>," $1 | tr '\n' '§' >> $2
+    while true; do
+        str=$(sed -ne 's,\*\*,<b>,' -e 's,\*\*,</b>,p' $2);
+        if [ -n "$str" ]; then
+            sed -e 's,\*\*,<b>,' -e 's,\*\*,</b>,' -i $2
+        else
+            break
+        fi
+    done
+    while true; do
+        str=$(sed -ne 's,\*,<i>,' -e 's,\*,</i>,p' $2);
+        if [ -n "$str" ]; then
+            sed -e 's,\*,<i>,' -e 's,\*,</i>,' -i $2
+        else
+            break
+        fi
+    done
+    while true; do
+        str=$(sed -ne 's,`,<tt>,' -e 's,`,</tt>,p' $2);
+        if [ -n "$str" ]; then
+            sed -e 's,`,<tt>,' -e 's,`,</tt>,' -i $2
+        else
+            break
+        fi
+    done
+    str=$(cat $2 | tr '§' '\n')
+    echo "$str
+    </body>
+</html>" > $2
+    
+}
+
 if [ "$2" != "" ]; then
     for i in "$@"; do
         bash $0 "$i"
@@ -20,42 +76,12 @@ mkdir -p html
 
 for i in *.md; do
     if [ "$i" == "README.md" ]; then
-        continue
+       echo "converting $i in index.html ..."
+       md2htmlfunc "$i" index.html
+       continue
     fi
     echo "converting $i in html ..."
-    f="html/${i%.md}.html"
-    echo "
-<html>
-    <head>
-        <title>${i%.md}</title>
-        <style>
-$(cat html/default.css)
-        </style>
-        <link rel='stylesheet' href='custom.css'>
-    </head>
-    <body>
-" >$f
-    cat $i >> $f
-
-    sed -e "s,^# \(.*\),<H1>\\1</H1>," \
--e "s,^## \(.*\),<H2>\\1</H2>," \
--e "s,^### \(.*\),<H3>\\1</H3>," \
--e "s,^#### \(.*\),<H4>\\1</H4>," \
--e "s,^##### \(.*\),<H5>\\1</H5>," \
--e "s,^ *[-+\*] *> *\(.*\),<blockquote><li>\\1</li></blockquote>," \
--e "s,^ *[-+\*] \(.*\),<li>\\1</li>," \
--e "s,^ *\([0-9]*\)\. \(.*\),<li style='list-style-type: none;'><b>\\1.</b><span>\&nbsp;\&nbsp;\&nbsp;</span>\\2</li>," \
--e "s,\*\*\(.*\)\*\*,<b>\\1</b>,g" \
--e "s,\*\(.*\)\*,<i>\\1</i>,g" \
--e "s,^---.*,<hr>," \
--e "s,^ *$,<p/>," \
--e "s,\\\<\(.*\)\\\>,\&lt;\\1\&gt;,g" \
--e "s,^> \(.*\),<blockquote>\\1</blockquote>," \
--e 's,\[\([^]]*\)\](\([^)]*\)),<a href="\2">\1<\/a>,g' \
--i $f
-    echo "
-    </body>
-</html>" >> $f
+    md2htmlfunc "$i" "html/${i%.md}.html"
 done
 
 echo
