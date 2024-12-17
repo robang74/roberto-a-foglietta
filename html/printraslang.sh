78c7a72267de0c1c2f1b0bc0300f192881c9d90a
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-12-17 21:08:00 +0100

    html/printraslang.sh: english->italian support added
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- html/printraslang.sh
+++ html/printraslang.sh
@@ -4,8 +4,11 @@
 #
 md=$1
 test -f "$md" || exit $?
+lg=it
+LG=IT
+link=https://raw-githubusercontent-com.translate.goog/robang74/roberto-a-foglietta/refs/heads/main
+echo "[[**\`${LG}\`**](${link}/${md}?_x_tr_sl=en&_x_tr_tl=${lg}&_x_tr_hl=${lg}-${LG}&_x_tr_pto=wapp)]"
 for lg in en de fr es; do
 LG=$(echo $lg | tr [a-z] [A-Z])
-link=https://raw-githubusercontent-com.translate.goog/robang74/roberto-a-foglietta/refs/heads/main
 echo "[[**\`${LG}\`**](${link}/${md}?_x_tr_sl=it&_x_tr_tl=${lg}&_x_tr_hl=${lg}-${LG}&_x_tr_pto=wapp)]"
 done
