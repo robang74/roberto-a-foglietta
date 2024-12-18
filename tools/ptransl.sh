ee4ddcff0fa9f651e003fe624437acc2a08b4655
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-12-18 17:20:28 +0100

    tools/ptransl.sh: improved
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- tools/ptransl.sh
+++ tools/ptransl.sh
@@ -3,8 +3,15 @@
 # (C) 2024, Roberto A. Foglietta <roberto.foglietta@gmail.com> - 3-clause BSD
 #
 
-link="https://raw-githubusercontent-com.translate.goog/robang74/roberto-a-foglietta/refs/heads/main"
-lraw="https://raw.githubusercontent.com/robang74/roberto-a-foglietta/refs/heads/main"
+gitusr="robang74"
+gitprj="roberto-a-foglietta"
+head="https://raw-githubusercontent"
+tail="refs/heads/main"
+body="${gitusr}/${gitprj}/${tail}"
+ext="com"
+
+link="${head}-${ext}.translate.goog/${body}"
+lraw="${head}.${ext}/${body}"
 
 if [ "x$1" == "x-s" ]; then
     ch1='' 
@@ -16,9 +23,11 @@ else
     
 fi
 test -f "$1" || exit $?; md=$1
-test -n "$ch1" &&
+if [ -n "$ch1" ]; then
+    title=$(head $md | sed -ne 's,^#  *\(.*\),\1,p' -e 's,^##  *\(.*\),\1,p' | head -n1)
+    echo "[${title:-\$TITLE}]($md)"
     echo '([**`raw`**]'"(${lraw}/$md))"
-
+fi
 lg=it
 LG=IT
 echo "${ch1}[**\`${LG}\`**](${link}/${md}?_x_tr_sl=en&_x_tr_tl=${lg}&_x_tr_hl=${lg}-${LG}&_x_tr_pto=wapp)${ch2}"
