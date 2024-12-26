40f081e9a8db135335f603c74228295df446e4c1
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2024-12-26 14:16:30 +0100

    tools/md2html.sh: parallel working version, imported
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- tools/md2html.sh
+++ tools/md2html.sh
@@ -2,10 +2,16 @@
 #
 # (C) 2024, Roberto A. Foglietta <roberto.foglietta@gmail.com> - 3-clause BSD
 #
+################################################################################
+
+declare -i start_t=$(date +%s%N)
+
+################################################################################
 
 li_A="<li style='list-style-type: none;'><b>"
 li_B=".</b><span>\&nbsp;\&nbsp;\&nbsp;</span>"
 #li_B=".<span style='visibility: hidden;'>_</span></b>"
+#li_B=".<span style='visibility: hidden;'>--</span></b>"
 
 ul_A="<ul class='dqt'><li class='dqt'><blockquote class='dqt'>"
 ul_B="</blockquote></li></ul>"
@@ -18,6 +24,7 @@ ul_B="</blockquote></li></ul>"
 
 function mini_mdlinkconv() {
     sed -e "s,\([ []*\)\[\([^][]*\)\]\([^(]\),\\1\&lbrack;\\2\&rbrack;\\3,g;" \
+        -e "s,\!\[\([^[]*\)](\([^)]*\)),<img src=\"\\2\" alt=\"\\1\">,g" \
         -e "s,\[\([^[]*\)](\([^)]*\)),<a href=\"\\2\">\\1</a>,g" "$@"
 }
 
@@ -26,7 +33,7 @@ function full_mdlinkconv() {
 }
 
 function md2htmlfunc() {
-    local i str=$(basename ${2%.html}) dir=""
+    local a b c i str=$(basename ${2%.html}) dir=""
     test "$str" == "index" && dir="html/"
 
     echo -n "<!DOCTYPE html>
@@ -100,15 +107,26 @@ function md2htmlfunc() {
     sed -e "s/<a [^>]*href=.http[^>]*/& target='_blank'/g" -i $2
     for i in 3 2 1; do
         let b=i*3 a=b-2 c=i+1; a=${a/1/2}; #echo "$i $a $b $c" >&2
-        sed -e "s/ \{$a,$b\}<li\([ >]\)/<li class='li${c}in'\\1/" -i $2
+        sed -e "s/ \{$a,$b\}<\(li\|blockquote\|tt\)\([ >]\)"\
+"/<\\1 class='li${c}in'\\2/" -i $2
     done
 }
 
-if [ "$2" != "" ]; then
-    for i in "$@"; do
-        bash $0 "$i"
+function get_images_list() {
+    local dir ext
+    for dir in "data/" "img/" ""; do
+        for ext in jpg png pdf; do
+            ls -1 ${dir}*.${ext}
+        done 2>/dev/null
     done
-else ###########################################################################
+}
+
+function link_md2html() {
+    local i="$1" f="$2" dir=""
+    test "$f" == "index.html" && dir="html/"
+    echo "$1" | grep -qe "^italian/" && dir="../italian/html/"
+    sed -e "s,\(href=[\"']\)$i\.md\([\"']\),\\1${dir}${i##*/}.html\\2,g" -i $f
+}
 
 zip=0
 if [ -d "$1" ]; then
@@ -121,66 +139,92 @@ elif [ "x$1" == "x-z" ]; then
     shift
 fi
 
-echo
+function main_md2html() {
+    local b=${2:-\`} a=${2:+}
+    local i j list="" index=0
+
+    printf "$a"
+    for i in $1; do
+        if [ "$i" == "template.md" ]; then
+            continue
+        elif [ "$i" == "README.md" ]; then
+            index=1
+            continue
+        fi
+        #echo "converting $i in html ..."
+        md2htmlfunc "$i" "html/${i%.md}.html"
+        list="$list html/${i%.md}.html"
+    done
+    if [ $index -ne 0 ]; then
+        #printf .
+        #echo "converting README.md in index.html ..."
+        md2htmlfunc README.md index.html
+    fi
+    printf "$b"
+
+    printf "$a"
+    #echo "redirection $1 image links ..."
+    for j in $list; do
+        for i in $(get_images_list); do
+            sed -e "s,\(href=.\)$i,\\1../$i,g" \
+                -e "s,\(src=.\)$i,\\1../$i,g" -i $j
+        done
+        for i in *.md; do
+            sed -e "s,\(href=.\)$i\">$i,\\1${i%.md}.html\">${i%.md}.html,g" -i $j
+        done
+    done
+    printf "$b" #2
+
+    printf "$a"
+    #echo "replacing $1 markdown links ..."
+    if [ "${PWD##*/}" == "chatgpt-answered-prompts" ]; then
+        index=0
+    fi
+    for i in $(ls -1 *.md italian/*.md 2>/dev/null); do
+        i=${i%.md}
+        test "$i" == "template" && continue
+        for j in $list; do
+            link_md2html $i $j
+        done
+        if [ $index -ne 0 ]; then
+            link_md2html $i index.html
+        fi
+    done
+    printf "$b" #3
+
+    printf "$a"
+    #echo "converting $1 markdown tables ..."
+    source tools/tabl2html.sh $list 2>/dev/null >&2
+    printf "$b" #4
+}
+
+################################################################################
+
 mkdir -p html
 test -n "$1" || rm -f html/[0-9]*.html
 
-list=""
-index=0
-for i in ${@:-*.md}; do
-    if [ "$i" == "template.md" ]; then
-        continue
-    elif [ "$i" == "README.md" ]; then
-        index=1
-        continue
-    fi
-    echo "converting $i in html ..."
-    md2htmlfunc "$i" "html/${i%.md}.html"
-    list="$list html/${i%.md}.html"
-done
-
+flist=$(ls -1 ${@:-*.md})
+declare -i n=$(echo "$flist" | wc -l)
 echo
-echo "redirecting html links ..."
-
-function get_images_list() {
-    local dir ext
-    for dir in "data/" "img/" ""; do
-        for ext in jpg png; do
-            ls -1 ${dir}*.${ext}
-        done 2>/dev/null
+if [ $n -gt 1 ]; then
+    let n*=4; str=$(seq 1 $n)
+    str=$(printf ".%.0s" $str)
+    printf "parallel working |$str|\n""work progression |"
+    for i in $flist; do
+        main_md2html $i &
+        sleep 0.1
     done
-}
+    wait
+    echo '|'
+else
+    printf "converting $1 "
+    main_md2html $1 '.'
+    printf " "
+fi
 
-for j in $list; do
-    for i in $(get_images_list); do
-        sed -e "s,\(href=.\)$i,\\1../$i,g" \
-            -e "s,\(src=.\)$i,\\1../$i,g" -i $j
-    done
-    for i in *.md; do
-        sed -e "s,\(href=.\)$i\">$i,\\1${i%.md}.html\">${i%.md}.html,g" -i $j
-    done
-done
-
-function link_md2html() {
-    local i=${1%.md} f="$2" dir=""
-    test "$f" == "index.html" && dir="html/"
-    sed -e "s,\(href=[\"']\)$i\.md\([\"']\),\\1${dir}$i.html\\2,g" -i $f
-}
-
-for i in *.md; do
-    #i=${i%.md}
-    #echo "$i" >&2
-    test "$i" == "template.md" && continue
-    for j in html/*.html index.html; do
-        #sed -e "s,\(href=[\"']\)$i\.md\([\"']\),\\1$i.html\\2,g" -i $j
-        link_md2html $i $j
-    done
-    #sed -e "s,\(href=[\"']\)$i\.md\([\"']\),\\1html/$i.html\\2,g" -i index.html
-done
-
-echo
-echo "converting md tables ..."
-source tools/tabl2html.sh $list 2>/dev/null
+let start_t=$(date +%s%N)-start_t
+let start_t=(start_t+500000)/1000000
+printf "done in %d.%03d ms\n\n" $[start_t/1000] $[start_t%1000]
 
 zipfle="archivio-html.zip"
 if [ "$zip" == "1" ]; then
@@ -189,6 +233,4 @@ if [ "$zip" == "1" ]; then
     zip -j $zipfle zip/README.md
     du -sk $zipfle
     echo
-fi
-
 fi
