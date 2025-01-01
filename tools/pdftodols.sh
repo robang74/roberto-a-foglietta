9823d45420a96f5c247bba152fb4f71f5316fbbe
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-01-01 10:21:54 +0100

    tools/pdftodols.sh: added new + pdf.todo/index.html: created
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

---
+++ tools/pdftodols.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+#
+# (C) 2024, Roberto A. Foglietta <roberto.foglietta@gmail.com> - 3-clause BSD
+#
+################################################################################
+
+dir="../html/"
+titleh="RAF pdf.todo folder"
+titleb="Roberto A. Foglietta"
+weburl="https://github.com/robang74/roberto-a-foglietta/blob/main/pdf.todo"
+downld="https://raw.githubusercontent.com/robang74/roberto-a-foglietta/refs/heads/main/pdf.todo"
+backlk="https://robang74.github.io/roberto-a-foglietta/index.html"
+target="target='_blank' rel='noopener noreferrer'"
+indexf="pdf.todo/index.html"
+tmpfle="pdf.todo/index.tmp"
+wpath=${indexf%/*}
+
+# readview 
+# download
+
+{ cd $wpath; ls -1 *.pdf; } | sort > $tmpfle
+sed -e "s,^.*$,<li>(<a href='$weburl/&' $target>\&hairsp;\&#128065;\&hairsp;</a>) \&middot;\&middot; \&#x21C3;<a href='${downld}/&' ${target}>\&#x1f4be;</a>\&#x21C2; \&middot;\&middot; &</li>," -i $tmpfle
+
+echo "<!DOCTYPE html>
+<html>
+    <head>
+        <title>${titleh}</title>
+        <meta charset='UTF-8'>
+        <link rel='stylesheet' href='${dir}default.css'>
+        <link rel='stylesheet' href='${dir}custom.css'>
+        <style>
+        a { text-decoration: none; }
+        </style>
+    </head>
+    <body>
+        <div id='firstdiv' created=':-99' style='max-width: 800px; margin: auto; white-space: pre-wrap; text-align: justify;'>
+            <h2 align='center'>${titleb}</h2><h3 align='center'>Index for <a href='${weburl}' ${target}>${wpath}</a> folder</h3><hr>"\
+> $indexf
+cat $tmpfle >> $indexf
+    echo "<hr><center>Surf back to the website's <b><a href='${backlk}'>main</a></b> page or for the <b><a href='#'>top</a></b> of this page.</center>
+        </div>
+        <br/>
+    </body>
+</html>" >> $indexf
+
+rm -f $tmpfle
