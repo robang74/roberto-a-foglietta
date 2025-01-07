96350ad69c511536e0f6f59f0458ed7ed16315cb
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-01-07 18:00:38 +0100

    html/items,ptopbar.sh: do not translate topbar and footnote
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- tools/ptopbar.sh
+++ tools/ptopbar.sh
@@ -4,6 +4,95 @@
 #
 ################################################################################
 
+function get_file_real_name() {
+    local file="${1:-}"
+    if [ "$file" == "test-page.md" ]; then
+        ln -sf tools/test-page.md .
+    elif [ "$file" == "index.html" ]; then
+        file="README.md"
+    fi
+    echo "$file"
+}
+
+function print_transl_from_to() {
+    echo "${gtlink}/${1}?_x_tr_sl=${2:-auto}&_x_tr_tl=${3}&_x_tr_hl=${3}-${4}"\
+"&_x_tr_pto=wapp"
+}
+
+function print_topbar() {
+    declare -A LANG_LINKS
+    local str lg LG lang=${7:-auto} trsl=0 file=${6:-}
+
+    if [ "$file" == "README.md" ]; then
+        file="index.html"
+    else
+        file="html/${file%.md}"
+    fi
+
+    PUBLISH_UNIVDATE="${3:-$(date +%F)}"
+    ALINK_CLASS="class='${LINE_SHADE} topbar'"
+
+    ORIGIN_CODE=""
+    PUBLISH_SOURCE="${4:-}"
+    if [ -n "$PUBLISH_SOURCE" ]; then
+        PUBLISH_LINK="${5:-}"
+        ORIGIN_CODE=" ${LINE_DASH} origin:&nbsp; 
+    <a $ALINK_CLASS href='${PUBLISH_LINK}'>${PUBLISH_SOURCE}</a>"
+    fi
+
+    declare -i skip=$lang
+    test $skip -ne 0 && lang="auto"
+    #echo "7: '$7', skip: '$kip', lang: '$lang'" >&2
+    if [ -n "$file" -a "$skip" != "-99" ]; then
+        lang=${lang,,}
+        TRNSL_STRN=""
+        for LG in IT EN DE FR ES; do
+            let skip++; test $skip -gt 0 || continue; lg=${LG,,}
+            if [ "$lang" != "$lg" ]; then
+                TRNSL_STRN+="<a $ALINK_CLASS "
+                str=$(print_transl_from_to "$file" $lang $lg $LG)
+                TRNSL_STRN+="href='$str'>${LG}</a>"
+                if [ "$LG" != "ES" ]; then TRNSL_STRN+=" ${MDOT_DASH} "; fi
+                trsl=1
+            fi
+        done
+    fi
+    if [ "$trsl" == "1" ]; then
+        TRNSL_STRN=" ${LINE_DASH} translate:&nbsp; ${TRNSL_STRN}"
+    else
+        TRNSL_STRN=""
+    fi
+
+    TOPBAR_STRING="<p/>
+<div class='topbar ${LINE_SHADE} ${TEXT_SHADE}' translate='no'>"\
+"&nbsp;${LINE_MARK} ${LINE_DASH} published:&nbsp; <b class='tpbrbold'>"\
+"${PUBLISH_UNIVDATE}</b> ${REVISION_STRING}${ORIGIN_CODE}${TRNSL_STRN}"
+
+    gotolist="1 2 3"
+    if [ "$file" != "index.html" ]; then
+        GOTO_LINKS[1,1]="../${GOTO_LINKS[1,1]}"
+        GOTO_LINKS[1,2]=".&#x27F0;."
+    fi
+
+    TOPBAR_STRING+=" ${LINE_DASH} goto:&nbsp;"
+    for i in $gotolist; do
+        TOPBAR_STRING+=" <a $ALINK_CLASS href='${GOTO_LINKS[$i,1]}'"\
+"$(test $i -ne 1 && printf target='_blank')>${GOTO_LINKS[$i,2]}</a>"
+        if [ $i -lt 3 ]; then TOPBAR_STRING+=" ${MDOT_DASH}"; fi
+    done
+    echo "${TOPBAR_STRING}&nbsp;</div>"
+
+}
+
+function get_html_item_str() {
+    if [ -r "$1" ]; then
+        str=$(cat "$1" | tr \" \')
+        eval echo \"$str\"
+    fi
+}
+
+################################################################################
+
 gitusr="robang74"
 gitprj="${PWD##*/}"
 weburl="https://${gitusr}.github.io"
@@ -20,9 +109,9 @@ fi
 
 LINE_SHADE="tpbr${color}"
 TEXT_SHADE="tpbrtext"
-LINE_MARK="&#9783;&nbsp;<b>&Ropf;</b>"
+LINE_MARK="<b>&#9783;&thinsp;&Ropf;</b>"
 LINE_DASH="&nbsp;&mdash;&nbsp;"
-LANG_DASH="&middot;"
+MDOT_DASH="&nbsp;<b>&middot;</b>&nbsp;"
 
 REVISION_STRING=""
 PUBLISH_UNIVDATE=""
@@ -32,24 +121,20 @@ PUBLISH_LINK=""
 declare -A PROJ_LINKS GOTO_LINKS
 
 PROJ_LINKS[1,1]="../chatbots-for-fun/index.html"
-PROJ_LINKS[1,2]="<tt>C4F</tt>"
+PROJ_LINKS[1,2]="C4F"
 PROJ_LINKS[2,1]="../roberto-a-foglietta/index.html"
-PROJ_LINKS[2,2]="<tt>RAF</tt>"
+PROJ_LINKS[2,2]="RAF"
 PROJ_LINKS[3,1]="../chatgpt-answered-prompts/index.html"
-PROJ_LINKS[3,2]="<tt>Q&A</tt>"
+PROJ_LINKS[3,2]="Q&A"
 
 ################################################################################
 
-file="${1:-}"
-if [ "$file" == "test-page.md" ]; then
-    ln -sf tools/test-page.md .
-fi
+file=$(get_file_real_name "${1:-}")
 test -r "$file" || exit 1
 
 dir=""
-if [ "$file" != "README.md" ]; then
+test "$file" != "README.md" && \
     dir="../"
-fi
 
 declare -i i n=2
 for i in 1 2 3; do
@@ -59,102 +144,19 @@ for i in 1 2 3; do
     let n++
 done
 GOTO_LINKS[1,1]="index.html#pages-index"
-GOTO_LINKS[1,2]="&#x2798;&#8505;."
+GOTO_LINKS[1,2]="&#x21b4;&hairsp;&#8505;&thinsp;&equiv;"
 
 if grep -qie "^#\{1,3\} *index *$" README.md 2>/dev/null; then
     GOTO_LINKS[1,1]="index.html#index"
 fi
 
-function print_transl_from_to() { ##############################################
-
-echo "${gtlink}/${1}?_x_tr_sl=${2:-auto}&_x_tr_tl=${3}&_x_tr_hl=${3}-${4}&_x_tr_pto=wapp"
-
-
-} ##############################################################################
-
-function print_topbar() { ######################################################
-
-declare -A LANG_LINKS
-local str lg LG lang=${7:-auto} trsl=0 file=${6:-}
-
-PUBLISH_UNIVDATE="${3:-$(date +%F)}"
-
-ORIGIN_CODE=""
-PUBLISH_SOURCE="${4:-}"
-if [ -n "$PUBLISH_SOURCE" ]; then
-    PUBLISH_LINK="${5:-}"
-    ORIGIN_CODE=" ${LINE_DASH} origin:&nbsp; <b class='tpbrbold tpbrlink'>
-<a class='${LINE_SHADE}' href='${PUBLISH_LINK}'>${PUBLISH_SOURCE}</a></b>"
-fi
-
-declare -i skip=$lang
-test $skip -ne 0 && lang="auto"
-#echo "7: '$7', skip: '$kip', lang: '$lang'" >&2
-if [ -n "$file" -a "$skip" != "-99" ]; then
-    lang=${lang,,}
-    TRNSL_STRN="<b class='tpbrlang tpbrbold tpbrlink'>"
-    for LG in IT EN DE FR ES; do
-        let skip++; test $skip -gt 0 || continue; lg=${LG,,}
-        if [ "$lang" != "$lg" ]; then
-            TRNSL_STRN+="<tt class='tpbrlang'><a class='${LINE_SHADE}' "
-            str=$(print_transl_from_to "$6" $lang $lg $LG)
-            TRNSL_STRN+="href='$str'>${LG}</a></tt>"
-            if [ "$LG" != "ES" ]; then TRNSL_STRN+=" ${LANG_DASH} "; fi
-            trsl=1
-        fi
-    done
-    TRNSL_STRN+="</b>"
-    #TRNSL_STRN="${TRNSL_STRN%</tt>*}</tt></b>"
-fi
-if [ "$trsl" == "1" ]; then
-    TRNSL_STRN=" ${LINE_DASH} translate:&nbsp; ${TRNSL_STRN}"
-else
-    TRNSL_STRN=""
-fi
-
-TOPBAR_STRING="<br/><div class='topbar ${LINE_SHADE} ${TEXT_SHADE}'>&nbsp;"\
-"${LINE_MARK} ${LINE_DASH} published:&nbsp; <b class='tpbrbold'>"\
-"${PUBLISH_UNIVDATE}</b>${REVISION_STRING}${ORIGIN_CODE}${TRNSL_STRN}"
-
-gotolist="1 2 3"
-if [ "$file" != "index.html" ]; then
-    GOTO_LINKS[1,1]="../${GOTO_LINKS[1,1]}"
-    GOTO_LINKS[1,2]=".&#x27F0;."
-fi
-
-TOPBAR_STRING+=" ${LINE_DASH} goto:&nbsp;<b class='tpbrbold tpbrlink'>"
-for i in $gotolist; do
-    TOPBAR_STRING+=" <a class='${LINE_SHADE}' href='${GOTO_LINKS[$i,1]}'"\
-"$(test $i -ne 1 && printf target='_blank')>${GOTO_LINKS[$i,2]}</a>"
-    if [ $i -lt 3 ]; then TOPBAR_STRING+=" ${LANG_DASH}"; fi
-done
-echo "${TOPBAR_STRING}</b>&nbsp;</div>"
-
-} ##############################################################################
-
-# print_topbar "dark" "warm" "$date" "Facebook" "https://facebook.com" "index.html" "it"
-# print_topbar "dark" "warm" "$date" "Facebook" "https://fbook.it" "html/test.html" "it"
-
 ################################################################################
 
-function get_html_item_str() {
-    if [ -r "$1" ]; then
-        str=$(cat "$1" | tr \" \')
-        eval echo \"$str\"
-    fi
-}
-
-file="${1:-}"
-if [ "$file" == "test-page.md" ]; then
-    ln -sf tools/test-page.md .
-fi
-if [ "$file" == "index.html" ]; then
-    file="README.md"
-fi
-test -r "$file" || exit 1
-
-eval set -- \
-$(sed -ne "s/<.* created=[\"']\([^:\"']*\):\([^:\"']*\):\{0,1\}\([^:\"']*\).*/'\\1' '\\2' '\\3'/p" "$file")
+c="created="
+s="'\\1' '\\2' '\\3'"
+eval set -- $(sed -ne \
+    "s/<.* $c[\"']\([^:\"']*\):\([^:\"']*\):\{0,1\}\([^:\"']*\).*/$s/p" "$file")
+#echo "1: '${1:-}', 2: '${2:-}', 3: '${3:-}'" >&2
 
 date1st=""
 declare -i DATETYPE=1 revnum=$[0${3:-}]
@@ -164,9 +166,9 @@ revnum+=$(echo "$gitlog" | grep . | wc -l)
 command git status -s "$file" | grep -q . && let revnum++
 
 if [ $revnum -gt 0 ]; then
-    REVISION_STRING=" ${LINE_DASH} revision: <b class='tpbrbold'
->${revnum}</b
->"
+    REVISION_STRING=" ${LINE_DASH} revision: <b class='tpbrbold
+'>${revnum}</b class='
+'>"
 fi 2>/dev/null
 
 date1st=${1:-}
@@ -180,11 +182,5 @@ if [ ! -n "$date1st" ]; then
 fi
 date1st+=$(get_html_item_str html/items/datetype.htm)
 
-if [ "$file" == "README.md" ]; then
-    file="index.html"
-else
-    file="html/${file%.md}"
-fi
-
-print_topbar "" "" "$date1st" "" "" "$file" "$2"
+print_topbar "" "" "$date1st" "" "" "$file" ${2:-}
 
