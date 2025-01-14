5c1b13b961069a8d1ad2ac2656b7c51cf2b61a81
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2025-01-14 17:23:57 +0100

    tools/pdfdolist.sh: updated with id='#tag' and html renew
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- tools/pdfdolist.sh
+++ tools/pdfdolist.sh
@@ -15,21 +15,21 @@ text_en='[&thinsp;<b><tt>EN</tt></b>&thinsp;] This folder contains ${FILE_NUMBER
 text_it='[&thinsp;<b><tt>IT</tt></b>&thinsp;] Questa cartella contiene ${FILE_NUMBER}files in PDF ${FILE_ACTION_IT}in markdown e poi in pagine html.'
 
 
-    for pdfdir in pdf.todo pdf.done; do
-        titleh="RAF $pdfdir folder"
-        weburl="${m_weburl}/$pdfdir"
-        downld="${m_downld}/$pdfdir"
-        indexf="$pdfdir/index.html"
-        tmpfle="$pdfdir/index.tmp"
-        
-        strn=$(cd $pdfdir >/dev/null; command ls -1 *.pdf)
-        numf=$(echo "$strn" | grep --color=never . | wc -l)
-        FILE_NUMBER="<b>$numf</b> "
+for pdfdir in pdf.todo pdf.done; do
+    titleh="RAF $pdfdir folder"
+    weburl="${m_weburl}/$pdfdir"
+    downld="${m_downld}/$pdfdir"
+    indexf="$pdfdir/index.html"
+    tmpfle="$pdfdir/index.tmp"
+    
+    strn=$(cd $pdfdir >/dev/null; command ls -1 *.pdf)
+    numf=$(echo "$strn" | grep --color=never . | wc -l)
+    FILE_NUMBER="<b>$numf</b> "
 
-echo "$strn" | sort > $tmpfle
-sed -e "s,^.*$,<li>(<a href='$weburl/&' $target>\&hairsp;\&#128065;\&hairsp;</a>) \&middot;\&middot; \&#x21C3;<a href='${downld}/&' ${target}>\&#x1f4be;</a>\&#x21C2; \&middot;\&middot; &</li>," -i $tmpfle
+    echo "$strn" | sort > $tmpfle
+    sed -e "s,^.*$,<li>(<a href='$weburl/&' $target>\&hairsp;\&#128065;\&hairsp;</a>) \&middot;\&middot; \&#x21C3;<a href='${downld}/&' ${target}>\&#x1f4be;</a>\&#x21C2; \&middot;\&middot; &</li>," -e "s,<li>\(.*; \)\([0-9a-z]*\)\(-[^<>]*</li>\),<li id='\\2'>\\1\\2\\3," -i $tmpfle
 
-echo -n "<!DOCTYPE html>
+    echo -n "<!DOCTYPE html>
 <html>
     <head>
         <title>${titleh}</title>
@@ -44,23 +44,23 @@ echo -n "<!DOCTYPE html>
         <div id='firstdiv' created=':-99' style='max-width: 800px; margin: auto; white-space: pre-wrap; text-align: justify;'>
             <h2 align='center'>${titleb}</h2><h3 align='center'>Index for <a href='${weburl}' ${target}>${pdfdir}</a> folder</h3><hr>"\
 > $indexf
-if [ "${pdfdir/pdf./}" == "todo" ]; then
-    FILE_ACTION_EN="<b>not yet converted</b> "
-    FILE_ACTION_IT="<b>da convertire</b> "
-elif [ "${pdfdir/pdf./}" == "done" ]; then
-    FILE_ACTION_EN="<b>already converted</b> "
-    FILE_ACTION_IT="<b>già convertiti</b> "
-fi
-strn="<br/><li>${text_it}</li><br/><li>${text_en}</li><br/><hr>" 
-eval echo \"$strn\" >> $indexf
-cat $tmpfle >> $indexf
+    if [ "${pdfdir/pdf./}" == "todo" ]; then
+        FILE_ACTION_EN="<b>not yet converted</b> "
+        FILE_ACTION_IT="<b>da convertire</b> "
+    elif [ "${pdfdir/pdf./}" == "done" ]; then
+        FILE_ACTION_EN="<b>already converted</b> "
+        FILE_ACTION_IT="<b>già convertiti</b> "
+    fi
+    strn="<br/><li>${text_it}</li><br/><li>${text_en}</li><br/><hr>" 
+    eval echo \"$strn\" >> $indexf
+    cat $tmpfle >> $indexf
     echo "<hr><center>Surf back to the website's <b><a href='${backlk}'>main</a></b> page or for the <b><a href='#'>top</a></b> of this page.</center>
         </div>
         <br/>
     </body>
 </html>" >> $indexf
 
-rm -f $tmpfle
+    rm -f $tmpfle
 
 done
 
